41dc5a8ffc4425a8225f8aafc09fa736ae0dc390
haruhi <h.mizuguchi1120@gmail.com>
2023-03-14 11:42:34 +0900

    引数等の細かい調整

--- run.py
+++ run.py
@@ -24,12 +24,12 @@ val_list = [1,3,5,7,9]
 cv_start = 0
 cv_end = 5
 
-method_list = ["PML-NI"]
-data_list = ['emotions']
-noise_list_multi = [50]
-val_list = [1]
+method_list = ["PML-VD"]
+data_list = ['music_emotion','enron','genbase']
+noise_list_multi = [50,100,150]
+val_list = [1,3,5,7,9]
 cv_start = 0
-cv_end = 1
+cv_end = 5
 
 
 for method in method_list:
@@ -43,13 +43,24 @@ for method in method_list:
                 for cv in range(cv_start,cv_end):
                     print(f"start method={method}, data={data}, noise={str(p_noise)}, val={str(p_val)}, cv={str(cv)}")
                     ### データの読み込み
-                    train_feature,train_cand,train_gt,test_feature,test_gt,train_val_feature,train_val_gt,train_noise_feature,train_noise_cand,batch_size = \
+                    train_feature,train_cand,train_gt,test_feature,test_gt,train_val_feature,train_val_gt,train_noise_feature,train_noise_cand,train_noise_gt,batch_size = \
                         read_data(data,p_noise,p_val,cv)
-
+                    
                     ### ニューラルネット用のデータ
                     if method in ["PML-VD","Baseline","PML-MD"]:
                         if method == "PML-VD":
-                            creds_pre = predict_creds(train_val_feature,train_val_gt,train_noise_feature,train_noise_cand)
+
+                            ### パラメータ
+                            # 信頼度の推定の方法を表す. (0,1,2)のいずれか
+                            # 0：検証集合のみで特徴を線形近似
+                            # 1：検証集合とノイズ付き集合のknnそれぞれで線形近似した後足し合わせる
+                            # 2：全ての訓練データのknnで線形近似
+                            mode = 0
+                            kind_dist = "cosine" # 距離関数("euclid","cosine")のどちらか
+                            k = 3 # knnのk
+                            alpha = -1
+
+                            creds_pre = predict_creds(train_val_feature,train_val_gt,train_noise_feature,train_noise_cand,mode,kind_dist,k,alpha=-1)
                         else: 
                             creds_pre = train_noise_cand
                         
@@ -106,3 +117,17 @@ for method in method_list:
                     ### 評価の表示
                     print(f"finish method={method}, data={data}, noise={str(p_noise)}, val={str(p_val)}, cv={str(cv)}")
                     eval_and_print(creds,predict_labels,test_gt)
+                    
+
+                    if method == "PML-VD":
+                        if mode == 0:
+                            method_for_save = "PML_VD_default"
+                        elif mode == 1:
+                            method_for_save = "PML_VD_plus"
+                        elif mode == 2:
+                            method_for_save = "PML_VD_all"
+                    else:
+                        method_for_save = method
+              
+                    np.savetxt(creds,f"result/{method_for_save}/{data}/{str(p_noise)}/true/{str(p_val)}/creds_{str(cv)}.csv",delimiter=",")
+                    np.savetxt(creds,f"result/{method_for_save}/{data}/{str(p_noise)}/true/{str(p_val)}/predict_labels_{str(cv)}.csv",delimiter=",")
